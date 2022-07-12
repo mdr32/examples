60f424353325432c8311cba8084f1fe941c1ec03
kichMan <kichmale@gmail.com>
2022-07-12 16:23:27 +0300

    исходник delay

---
+++ src/delay.c
@@ -0,0 +1,32 @@
+#include "delay.h"
+
+/**
+ * @brief Процессор имеет 24-х разрядный системный таймер, SysTick
+ */
+volatile uint16_t _millis;
+
+static uint16_t millis(void) {
+    return _millis;
+}
+
+/**
+ * @brief Обработчик прерывания из таблицы прерываний
+ */
+void SysTick_Handler(void)
+{
+    _millis++;
+}
+
+void delay_setup(void)
+{
+	systick_set_clocksource(STK_CSR_CLKSOURCE_AHB);
+    systick_clear();
+	systick_set_reload(rcc_ahb_frequency / 1000 - 1);
+    systick_counter_enable();
+	systick_interrupt_enable();
+}
+
+void delay_ms(uint16_t delay){
+    const uint16_t until = millis() + delay;
+    while (millis() < until);
+}
