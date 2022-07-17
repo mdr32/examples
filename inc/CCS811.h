7e730b9aa426fad7538d61a152ba2d1a5981350d
kichMan <kichmale@gmail.com>
2022-07-17 18:26:29 +0300

    дефолтные классы

---
+++ inc/CCS811.h
@@ -0,0 +1,16 @@
+#ifndef _CCS811_H
+#define _CCS811_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+
+void CCS811_data_read (void);
+void CCS811_data_calc (void);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
---
+++ src/CCS811.c
@@ -0,0 +1,6 @@
+#include "CCS811.h"
+
+
+
+void CCS811_data_read (void) {}
+void CCS811_data_calc (void) {}
